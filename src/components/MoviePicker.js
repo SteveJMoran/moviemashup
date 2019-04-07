779475048a42977953456a3fa998f205ce936d71
Steven Moran <stevenmoran@Stevens-iMac.local>
2019-04-07 11:08:30 -0400

    inital comit with movie picker start

---
+++ src/components/MoviePicker.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { throttle, debounce } from 'throttle-debounce';
+
+const API_TOKEN = process.env.REACT_APP_API_KEY
+
+class MoviePicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { q: "", results: []};
+    this.autocompleteSearchDebounced = debounce(300, this.autocompleteSearch);
+    this.autocompleteSearchThrottled = throttle(300, this.autocompleteSearch);
+  }
+
+  changeQuery = event => {
+    this.setState(
+      {q: event.target.value},
+      () => {
+        const q = this.state.q;
+        if(q.length < 5) {
+          this.autocompleteSearchThrottled(this.state.q);
+        } else {
+          this.autocompleteSearchDebounced(this.state.q);
+        }
+      }
+    )
+  }
+
+  autocompleteSearch = q => {
+    this.fetch(q);
+  }
+
+  fetch = q => {
+    const _searches = this.state._searches || [];
+    _searches.push(q);
+    this.setState({_searches});
+
+    this.searchMovies(q);
+  }
+  async searchMovies (q) {
+    try {
+
+      const qUrl = "https://api.themoviedb.org/3/search/movie";
+      const qParams = {
+        crossDomain: true,
+        api_key: API_TOKEN,
+        query: q,
+        page: 1,
+        include_adult: false
+      }
+      if(q.length){
+        const movieData = await axios.get(qUrl, {params: qParams})
+        const { results: movies } = movieData.data;
+
+        this.setState({results: movies})
+      }
+    } catch(e) {
+      console.error(e.message)
+    }
+  }
+  renderSearchResults() {
+    const { results } = this.state;
+    const resultsHtml = results.map(result => {
+      const {
+        id,
+        title,
+        release_date: date
+      } = result;
+    const year = parseInt(date);
+
+      return(
+        <div 
+        key={id}>
+          { title } ({year})
+        </div>
+      )
+    })
+    return resultsHtml;
+  }
+  render() {
+    return (
+      <div>
+        <input
+          placeholder="Pick a movie"
+          type="text"
+          onChange={ this.changeQuery }
+        /> 
+        <div className="movieAutoResults">
+          { this.state.results.length ? this.renderSearchResults() : "Loading.." }
+        </div> 
+      </div>
+    )
+  }
+
+}
+
+export default MoviePicker;
